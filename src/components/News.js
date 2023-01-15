98bade97842c4e10a5874d482a3cc7ac8898f3b5
Sejal kathane <kathanesejal@gmail.com>
2023-01-15 21:04:20 +0530

    modify img

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,25 @@ import NewsItem from "./NewsItem";
 export class News extends Component {
     render() {
         return (
-            <div>
-                This is a News Component
+            <div className="container my-3">
+               <h2>News Keen Top Headline</h2>
+               
+               <div className="row">
+
+                <div className="col-md-4">
+                <NewsItem title="myTitle" description="myDescription" imageUrl="https://media.cnn.com/api/v1/images/stellar/prod/230115050553-02-pokhara-plane-crash-011523.jpg?c=16x9&q=w_800,c_fill" />
+                </div>
+
+                <div className="col-md-4">
                 <NewsItem title="myTitle" description="myDescription"  />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
+                </div>
+
+                <div className="col-md-4">
+                <NewsItem title="myTitle" description="myDescription"  />
+                </div>
+
+               </div>
+
             </div>
         );
     }
